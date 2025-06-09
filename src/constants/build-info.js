231dba66066d1d47373c462cf31f3cec398fcd1e
officialashraf <officialashraf7804@gmail.com>
2025-06-09 11:51:25 +0530

    add buil.js dockerization completed mvp 1.1

---
+++ src/constants/build-info.js
@@ -0,0 +1,8 @@
+export default {
+  "productName": "opensearch",
+  "version": "0.1.0",
+  "lastModifiedOn": "Mon Jun 9 11:10:38 2025 +0530",
+  "updatedBy": "officialashraf",
+  "commit": "678fe1d",
+  "maintainedBy": "Ashraf Shaikh"
+};
