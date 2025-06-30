a609a12dc2aca2437cab6c25495cefbf286fb891
officialashraf <officialashraf7804@gmail.com>
2025-06-30 19:08:07 +0500

    Dashboard Implemented

--- src/utils/appContent.js
+++ src/utils/appContent.js
@@ -6,9 +6,9 @@ import MainFilter from '../Component/Modules/Summarymain/mainFilterPage.js';
 import AddFilter2 from '../Component/Modules/Filters/addFilter.js';
 import CaseTableDataFilter from '../Component/Modules/Analyze/caseTableDataFilter.js';
 import CaseAddFilter from '../Component/Modules/Summarymain/unusable/caseAddFilter.js';
-import RightSidebar from '../Component/Modules/Home/rightSideBar.js';
+// import RightSidebar from '../Component/Modules/Home/rightSideBar.js';
 import Sidebar from '../Component/Modules/Home/leftSideBar.js';
-import '../Component/Modules/Home/dashboard.css';
+import styles from '../Component/Modules/Home/dashboard.module.css';
 import Summary from '../Component/Modules/Summarymain/summary.js';
 import LineChart1 from '../Component/Modules/Analyze/GraphicalData/lineChart.js';
 import KeywordChart from '../Component/Modules/Analyze/GraphicalData/keywordChart.js';
@@ -65,19 +65,19 @@ const AppContent = () => {
     };
     const excludedPaths = ["/login", "/", "/license"];
     return (
-        <div className="ContainerDashboard">
+        <div className={styles.ContainerDashboard}>
             {/* Render Header for all pages except the LoginPage */}
 
-            <div className="dashboard-container">
-                <div className="cont-d">
+            <div className={styles.dashboardContainer}>
+                <div className={styles.contD}>
                     {!excludedPaths.includes(location.pathname) && <Header title={getHeaderTitle()} />}
 
                 </div>
-                <div className="cont-a">
+                <div className={styles.contA}>
                     {!excludedPaths.includes(location.pathname) && <Sidebar />}
                 </div>
 
-                <div className="cont-b">
+                <div className={styles.contB}>
                     <Routes>
                         <Route path='/' element={<LicenseValidator />} />
                            <Route path='/btn' element={<DAButton />} />
@@ -114,9 +114,9 @@ const AppContent = () => {
                     </Routes>
                 </div>
 
-                <div className="cont-c">
+                {/* <div className="cont-c">
                     {!excludedPaths.includes(location.pathname) && <RightSidebar />}
-                </div>
+                </div> */}
             </div>
         </div>
     );
